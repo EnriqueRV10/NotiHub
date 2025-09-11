805bd0606119eaf97a17def3f2a17ab97a87d0fc
EnriqueRV10 <sergioenri2001@gmail.com>
2025-09-11 14:47:56 -0600

    feat: Se agregó pagina de Estadisticas y se integró un menu de navegacion en el Sider de RootLatyout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { ConfigProvider, Layout, Menu } from "antd";
+import { ConfigProvider, Layout, Menu, MenuProps } from "antd";
 import React, { useState } from "react";
 import esEs from "antd/locale/es_ES";
+import { useRouter, usePathname } from "next/navigation";
+import { HomeOutlined, PieChartOutlined } from "@ant-design/icons";
+import path from "path";
 
 const queryClient = new QueryClient();
 
@@ -14,12 +17,42 @@ const inter = Inter({ subsets: ["latin"] });
 
 const { Header, Content, Sider } = Layout;
 
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState(true);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const items: MenuItem[] = [
+    getItem("Inicio", "/", <HomeOutlined />),
+    getItem("Datos", "/Datos", <PieChartOutlined />),
+  ];
+
+  const handleMenuClick = (e: { key: string }) => {
+    if (pathname !== e.key) {
+      router.push(e.key);
+    }
+  };
+
   return (
     <html lang="es">
       <body className={inter.className}>
@@ -33,10 +66,24 @@ export default function RootLayout({
                   onCollapse={(value) => setCollapsed(value)}
                 >
                   <div
-                    style={{ color: "white", padding: 16, textAlign: "left" }}
+                    style={{
+                      color: "white",
+                      padding: 16,
+                      textAlign: "left",
+                      fontWeight: "bold",
+                      fontSize: 18,
+                    }}
                   >
                     NotiHub
                   </div>
+
+                  <Menu
+                    theme="dark"
+                    defaultSelectedKeys={[pathname]}
+                    mode="inline"
+                    items={items}
+                    onClick={handleMenuClick}
+                  />
                 </Sider>
                 <Layout>
                   <Header
